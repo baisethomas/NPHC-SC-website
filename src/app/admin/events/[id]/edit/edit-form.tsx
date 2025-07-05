f92c0caac65f79e45ffc0c3a4d1e7839edfc1e28
Baise Thomas <baise.thomas@gmail.com>
2025-07-05 23:21:20 +0000

    some UI changes. make sure the Date and Time are in alignment

--- src/app/admin/events/[id]/edit/edit-form.tsx
+++ src/app/admin/events/[id]/edit/edit-form.tsx
@@ -128,7 +128,7 @@ export function EditEventForm({ event }: { event: Event }) {
                 <FormMessage />
               </FormItem>
             )} />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
               <FormField
                 control={form.control}
                 name="date"
